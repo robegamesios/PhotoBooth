2d0305a0bcf70266f1260e985a685a23bc99d1c0
Rob Enriquez <rob@goabra.com>
2016-10-17 01:43:20 -0700

    add introViewController

---
+++ PhotoBooth/PhotoBooth/IntroViewController.m
@@ -0,0 +1,37 @@
+//
+//  IntroViewController.m
+//  PhotoBooth
+//
+//  Created by Rob Enriquez on 10/17/16.
+//  Copyright © 2016 robert enriquez. All rights reserved.
+//
+
+#import "IntroViewController.h"
+
+@interface IntroViewController ()
+
+@end
+
+@implementation IntroViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+
+    NSNumber *value = [NSNumber numberWithInt:UIInterfaceOrientationLandscapeRight];
+    [[UIDevice currentDevice] setValue:value forKey:@"orientation"];
+}
+
+- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation{
+    return UIInterfaceOrientationLandscapeRight;
+}
+
+- (BOOL) shouldAutorotate {
+    return NO;
+}
+
+@end
